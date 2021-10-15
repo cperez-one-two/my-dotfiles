8dd25fca09c1f71f0cfb451ab6374ccfd06f6f0e
cperez-one-two <christopher12perez6@gmail.com>
2021-10-15 00:52:05 -0400

    added bspwm and polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
+
+outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+tray_output=eDP-1
+
+
+for m in $outputs; do
+	if [[ $m == "HDMI-2" ]]; then
+		tray_output=$m
+	fi
+done
+
+for m in $outputs ; do
+	if [[ $m == $tray_output ]]; then
+		TRAY_POSITION=left MONITOR=$m polybar --reload example 2>~/.cache/polybar/$m.log &
+	else
+		TRAY_POSITION=none MONITOR=$m polybar --reload example 2>~/.cache/polybar/$m.log &
+	fi
+done
+#polybar --reload example 2>~/.cache/polybar/$m.log &
