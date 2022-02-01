4c9bfad7dddf534a05ae3902fffa37e88e31991d
cperez-one-two <christopher12perez6@gmail.com>
2022-01-31 21:48:12 -0500

    chore linting

--- emacs/init-new.el
+++ emacs/init-new.el
@@ -18,16 +18,16 @@
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; font
-(defvar efs/default-font-size 140)
-(defvar efs/default-variable-font-size 140)
+ (defvar efs/default-font-size 140)
+ (defvar efs/default-variable-font-size 140)
 
-;; later used to configure UI elements
-(set-face-attribute 'default nil :font "TerminessTTF Nerd Font Mono" :height efs/default-font-size)
-(set-face-attribute 'fixed-pitch nil :font "TerminessTTF Nerd Font Mono" :height efs/default-font-size)
-(set-face-attribute 'variable-pitch nil :font "TerminessTTF Nerd Font Mono" :height efs/default-variable-font-size :weight 'medium)
-;; (set-face-attribute 'default nil :font "Iosevka Nerd Font Mono" :height efs/default-font-size)
-;; (set-face-attribute 'fixed-pitch nil :font "Iosevka Nerd Font Mono" :height efs/default-font-size)
-;; (set-face-attribute 'variable-pitch nil :font "Iosevka" :height efs/default-variable-font-size :weight 'medium)
+ ;; later used to configure UI elements
+ ;; (set-face-attribute 'default nil :font "TerminessTTF Nerd Font Mono" :height efs/default-font-size)
+ ;; (set-face-attribute 'fixed-pitch nil :font "TerminessTTF Nerd Font Mono" :height efs/default-font-size)
+ ;; (set-face-attribute 'variable-pitch nil :font "TerminessTTF Nerd Font Mono" :height efs/default-variable-font-size :weight 'medium)
+(set-face-attribute 'default nil :font "Iosevka Nerd Font Mono" :height efs/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Iosevka Nerd Font Mono" :height efs/default-font-size)
+(set-face-attribute 'variable-pitch nil :font "Iosevka" :height efs/default-variable-font-size :weight 'medium)
 
 ;; Initialize package repos
 (require 'package)
@@ -57,6 +57,144 @@
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
   (setq ivy-wrap t))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; NOTE: The fist time you load this on a new machine, be sure to run:
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package doom-themes
+  ;;:init (load-theme 'doom-snazzy t)
+  ;;:init (load-theme 'doom-gruvbox t)
+  ;;:init (load-theme 'doom-horizon t)
+  ;;:init (load-theme 'doom-palenight t)
+  :init (load-theme 'doom-tomorrow-night t))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1.2))
+
+(use-package general
+  :config
+  (general-create-definer efs/leader-key
+    :prefix "C-c")
+
+  (efs/leader-key
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("n" text-scale-increase "up")
+  ("p" text-scale-decrease "down")
+  ("q" nil "finish and exit" :exit t))
+
+(efs/leader-key
+  "ts"  '(hydra-text-scale/body :which-key "scale text"))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/projects")
+    (setq projectile-project-search-path '("~/projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :after projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; forge: allows you to use a lot of github features from within emacs
+;; (use-package forge)
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  ;(variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t)
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+  (setq org-agenda-files
+        '("~/dotfiles/emacs/hello.org"
+          "~/dotfiles/emacs/birthdays.org")))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(with-eval-after-load 'org-faces
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Iosevka" :weight 'medium :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
 
 (with-eval-after-load 'org
   (org-babel-do-load-languages
